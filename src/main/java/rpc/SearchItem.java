84431089267546911d9899625b19a2cda8a75dce
SherlockVWayne <haoxiang@6554@gmail.com>
2020-08-14 22:15:59 -0700

    My first change

---
+++ src/main/java/rpc/SearchItem.java
@@ -0,0 +1,79 @@
+package rpc;
+
+import java.io.*;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import external.GitHubClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+import entity.Item;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+//        JSONArray array = new JSONArray();
+//        array.put(new JSONObject().put("username", "abcd"));
+//        array.put(new JSONObject().put("username", "1234"));
+//
+//        RpcHelper.writeJsonArray(response, array);
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		GitHubClient client = new GitHubClient();
+		List<Item> itemList = client.search(lat, lon, null);
+		
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+		
+		JSONArray array = new JSONArray();
+		for (Item item : itemList) {
+			JSONObject obj = item.toJSONObject();
+			obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);	}
+   
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) 
+	 */ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
